0a7bab794514686dfcd0c873943745794de3f699
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-29 16:40:08 +0500

    Fixed Display background Color

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/frontend-assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate =useNavigate()
   return (
     <>
     <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-            <img className='w-8 bg-black rounded-2xl p-2 cursor-pointer' src={assets.arrow_left} alt="" />
-            <img className='w-8 bg-black rounded-2xl p-2 cursor-pointer' src={assets.arrow_right} alt="" />
+            <img onClick={()=>navigate(-1)} className='w-8 bg-black rounded-2xl p-2 cursor-pointer' src={assets.arrow_left} alt="" />
+            <img onClick={()=>navigate(+1)} className='w-8 bg-black rounded-2xl p-2 cursor-pointer' src={assets.arrow_right} alt="" />
         </div>
         <div className="flex items-center gap-4">
             <p className='bg-white text-black px-4 py-1 text-[15px] cursor-pointer rounded-2xl hidden md:block'>Explore Premium</p>
