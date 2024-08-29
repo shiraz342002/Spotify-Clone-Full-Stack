a617a655b8d29b5e2f7f28017383be3259347ad4
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-29 16:09:55 +0500

    Display Page specific albums with details created

--- Front-End/src/components/DisplayAlbum.jsx
+++ Front-End/src/components/DisplayAlbum.jsx
@@ -1,38 +1,59 @@
 import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
-import { albumsData, assets } from '../assets/frontend-assets/assets'
+import { albumsData, assets, songsData } from '../assets/frontend-assets/assets'
 
 const DisplayAlbum = () => {
-    const {id} = useParams()
-    const albumData=albumsData[id]
+    const { id } = useParams()
+    const albumData = albumsData[id]
 
-  return (
-    <>
-    <Navbar/>
-    <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
-        <img className='w-48 rounded' src={albumData.image} alt="" />
-        <div className='flex flex-col'>
-            <p>Playlist</p>
-            <h2 className='text-5xl font-bold mb-4 md:text-8xl '>{albumData.name}</h2>
-            <h4>{albumData.desc}</h4>
-            <p className='mt-1'>
-                <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
-                <b> Spotify </b>
-                • 2,3694 likes
-                • <b>50 songs, </b>
-                about 2 hour 18 mins
-            </p>
-        </div>
-        </div>
-        <div className="grid grid-col-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
-            <p><b className='mr-4'>#</b>Title</p>
-            <p>Album</p>
-            <p className='hidden sm:block'>Date Added</p>
-            <img className='m-auto w-4' src={assets.clock_icon} alt="" />
-    </div>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <div className="mt-10 flex gap-8 flex-col md:flex-row md:items-end">
+                <img className='w-48 rounded' src={albumData.image} alt="" />
+                <div className='flex flex-col'>
+                    <p>Playlist</p>
+                    <h2 className='text-5xl font-bold mb-4 md:text-8xl '>{albumData.name}</h2>
+                    <h4>{albumData.desc}</h4>
+                    <p className='mt-1'>
+                        <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+                        <b> Spotify </b>
+                        • 2,3694 likes
+                        • <b>50 songs, </b>
+                        about 2 hour 18 mins
+                    </p>
+                </div>
+            </div>
+            <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+                <p><b className='mr-4'>#</b>Title</p>
+                <p>Album</p>
+                <p className='hidden sm:block'>Date Added</p>
+                <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+            </div>
+            <hr />
+            {
+                songsData.map((item, index) => (
+                    <div key={index} className='grid grid-cols-3 sm:grid-cols-4 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+                        <p className='text-white'>
+                            <b className='mr-4 text-[#a7a7a7]'>{index + 1}</b>
+                            <img className='inline w-10 mr-5' src={item.image} alt="" />
+                            {item.name}
+                        </p>
+                        <p className='text-[15px]'>
+                            {albumData.name}
+                        </p>
+                        <p className='text-[15px] hidden sm:block'>
+                            5 days ago
+                        </p>
+                        <p className='text-[15px] m-auto sm:text-right'>
+                            {item.duration}
+                        </p>
+                    </div>
+                ))
+            }
+        </>
+    )
 }
 
 export default DisplayAlbum
