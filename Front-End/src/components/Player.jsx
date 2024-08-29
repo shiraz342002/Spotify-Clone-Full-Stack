10a1e60218c67b127f5805b1aba9ad2f83a982d2
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-29 11:53:51 +0500

    Play funcionality added

--- Front-End/src/components/Player.jsx
+++ Front-End/src/components/Player.jsx
@@ -18,6 +18,24 @@ const Player = () => {
             <img className='cursor-pointer w-4' src={assets.next_icon} alt="" />
             <img className='cursor-pointer w-4' src={assets.loop_icon} alt="" />
         </div>
+        <div className="flex items-center gap-5">
+            <p>1:03</p>
+        <div className="w-[60vw] bg-gray-300 max-w-[500px] rounded-full cursor-pointer">
+            <hr className='h-1 border-none bg-green-700 w-20 rounded-full' />
+        </div>
+        <p>3:20</p>
+        </div>
+      </div>
+      <div className='hidden lg:flex items-center gap-3 opacity-75 '>
+        <img className='w-4' src={assets.plays_icon} alt="" />
+        <img className='w-4' src={assets.mic_icon} alt="" />
+        <img className='w-4' src={assets.queue_icon} alt="" />
+        <img className='w-4' src={assets.speaker_icon} alt="" />
+        <img className='w-4' src={assets.volume_icon} alt="" />
+        <div className='w-20 bg-slate-50 h-1 rounded'>
+        </div>
+        <img className='w-4' src={assets.mini_player_icon} alt="" />
+        <img className='w-4' src={assets.zoom_icon} alt="" />
       </div>
     </div>
   )
