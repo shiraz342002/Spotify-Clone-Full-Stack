e166f511894163423215ef0e0c6fd31c3daccafc
Shiraz Mazhar <imshiraz007@gmail.com>
2024-08-30 12:10:01 +0500

    Play Song pause , play functionality added

--- Front-End/src/context/PlayerContext.jsx
+++ Front-End/src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { assets, songsData } from "../assets/frontend-assets/assets";
 
 export const PlayerContext =createContext();
@@ -27,6 +27,23 @@ const PlayerContextProvider=(props)=>{
         audioRef.current.pause();
         setPlayStatus(false)
     }
+    useEffect(()=>{
+        setTimeout(() => {
+            audioRef.current.ontimeupdate=()=>{
+                seekBar.current.style.width=(Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
+                setTime({
+                    currentTime:{
+                        second:Math.floor(audioRef.current.currentTime%60),
+                        minute:Math.floor(audioRef.current.currentTime/60),
+                    },
+                    totalTime:{
+                         second:Math.floor(audioRef.current.duration%60),
+                        minute:Math.floor(audioRef.current.duration/60),
+                    }
+                })
+            }
+        }, 1000);
+    },[audioRef])
         
     const contextValue={
         audioRef,
