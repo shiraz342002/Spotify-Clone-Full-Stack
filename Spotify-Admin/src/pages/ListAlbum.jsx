8117e53ac4618a1cb74be6a08fe50f9c5f1d1316
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-06 15:22:26 +0500

    View Album Added for admin panel

--- Spotify-Admin/src/pages/ListAlbum.jsx
+++ Spotify-Admin/src/pages/ListAlbum.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { url } from '../App'
+import { toast } from 'react-toastify'
 
 const ListAlbum = () => {
+  const [data, setData] = useState([])
+  const fetchAlbums = async () => {
+    try {
+      const response = await axios.get(`${url}/api/album/list`)
+      if (response.data.success) {
+        setData(response.data.albums)
+      }
+    } catch (error) {
+      toast.error("Some Error occured")
+    }
+  }
+  useEffect(() => {
+    fetchAlbums()
+  }, [data])
+
+  const removeAlbum = async (id) => {
+    
+    try {
+      const response = await axios.delete(`${url}/api/album/delete/${id}`)
+      if (response.data.success) {
+        toast.success("Album Deleted Successfully")
+      }
+    } catch (error) {
+      toast.error("Some Error Occured")
+    }
+  }
   return (
     <div>
       
