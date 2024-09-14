65b0095e10ddf17994096b646866642bbc401fa0
Shiraz Mazhar <imshiraz007@gmail.com>
2024-09-14 21:20:51 +0500

    Fixed Albums Images width

--- Front-End/src/components/AlbumItem.jsx
+++ Front-End/src/components/AlbumItem.jsx
@@ -5,7 +5,7 @@ const AlbumItem = ({image,name,desc,id}) => {
   const navigate = useNavigate()
   return (
     <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[180px] px-3 p-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-      <img className='rounded' src={image} alt="" />
+      <img className='rounded w-72' src={image} alt="" />
       <p className='font-bold mt-2 mb-1 '>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
--- Front-End/src/components/SongItem.jsx
+++ Front-End/src/components/SongItem.jsx
@@ -5,7 +5,7 @@ const SongItem = ({name,image,desc,id}) => {
   const {playWithId} = useContext(PlayerContext)
   return (
     <div onClick={()=>playWithId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-      <img className='rounded' src={image} alt="" />
+      <img className='rounded w-72' src={image} alt="" />
       <p className='mt-2 mb-1 font-bold'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
